6469bb38e3dbb920f74f02e83b56fcb9f7ec87d9
Matheus de Assis Alcântara <matheus.labsit@dotzmkt.com.br>
2022-08-02 22:10:24 -0300

    create

---
+++ CreateDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Cosmos;
+
+var cosmosEndpointUri = "https://appaccount.documents.azure.com:443/";
+var cosmosDBKey = "riZtEhsY2qeICNuHNPX3FoJJHxcVB9rXnGHx7M4LPAsinGH8iqBstzzW5EYy0rioWh8ILY7okxc0BJdtQFXDrw==";
+
+await CreateDatabase("appdb");
+
+async Task CreateDatabase(string databaseName)
+{
+    var cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+    await cosmosClient.CreateDatabaseAsync(databaseName);
+    Console.WriteLine("Database created");
+}
