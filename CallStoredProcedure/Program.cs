17716bf72d918f32e464c9d719d8c360c4712c05
Matheus de Assis Alcântara <assis4002@gmail.com>
2022-08-04 23:46:32 -0300

    exec procedure

---
+++ CallStoredProcedure/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Cosmos;
+
+var cosmosEndpointUri = "https://appaccount.documents.azure.com:443/";
+var cosmosDBKey = "riZtEhsY2qeICNuHNPX3FoJJHxcVB9rXnGHx7M4LPAsinGH8iqBstzzW5EYy0rioWh8ILY7okxc0BJdtQFXDrw==";
+var databaseName = "appdb";
+var containerName = "Orders";
+
+await CallStoredProcedure();
+
+async Task CallStoredProcedure()
+{
+    var cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+    var container = cosmosClient.GetContainer(databaseName, containerName);
+    var result = await container.Scripts.ExecuteStoredProcedureAsync<string>("Display", new PartitionKey(""), null);
+    
+    Console.WriteLine(result);
+}
+
